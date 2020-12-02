83452f315aa7eec8284ec03c1b6d5ccd0586c065
dkovalenko0 <dimakovalenko715@gmail.com>
2020-12-02 17:22:02 +0200

    add animation

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,49 @@
-$(function(){
+const animItems = document.querySelectorAll('._anim-items');
+const navLinks = document.querySelectorAll('.nav__link')
 
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+    function animOnScroll() {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
 
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
 
-  
-});
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('_active');
+
+                if (animItem.classList.contains('work__content')) {
+                    for (let i = 0; i < navLinks.length; i++) {
+                        const navLink = navLinks[i];
+                        navLink.classList.add('nav__link--black')
+                    }
+                }
+            } else {
+                if (!animItem.classList.contains('_anim-no-hide')) {
+                    animItem.classList.remove('_active');
+                }
+
+                for (let i = 0; i < navLinks.length; i++) {
+                    const navLink = navLinks[i];
+                    navLink.classList.remove('nav__link--black')
+                }
+            }
+        }
+    }
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    }
+
+    setTimeout(() => {
+        animOnScroll();
+    }, 300);
+}
